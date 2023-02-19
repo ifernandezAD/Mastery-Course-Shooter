31dbe7676356283e2609b91995bc0970f6225958
IFernandezAD <ifernandezad@gmail.com>
2023-02-19 16:09:29 +0100

    Editor Stuff

--- Assets/Scripts/Audio/SimpleAudioEvent.cs
+++ Assets/Scripts/Audio/SimpleAudioEvent.cs
@@ -5,7 +5,14 @@ using UnityEngine;
 [CreateAssetMenu ( menuName = "Audio Events/Simple")]
 public class SimpleAudioEvent : ScriptableObject
 {
-    [SerializeField] private AudioClip[] clips = new AudioClip[0];
+    [SerializeField] 
+    private AudioClip[] clips = new AudioClip[0];
+    [SerializeField] 
+    private RangedFloat volume = new RangedFloat(1, 1);
+    [SerializeField]
+    [MinMaxRange(0f, 2f)]
+    private RangedFloat pitch = new RangedFloat(1, 1);
+
 
     public void Play(AudioSource source)
     {
