64b7aed023e1e6639052966337e262fc9101c67b
IFernandezAD <ifernandezad@gmail.com>
2023-02-19 16:24:59 +0100

    Updated Scriptable Audio objects

--- Assets/Scripts/Audio/SimpleAudioEvent.cs
+++ Assets/Scripts/Audio/SimpleAudioEvent.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Audio;
 
 [CreateAssetMenu ( menuName = "Audio Events/Simple")]
 public class SimpleAudioEvent : ScriptableObject
@@ -12,12 +13,26 @@ public class SimpleAudioEvent : ScriptableObject
     [SerializeField]
     [MinMaxRange(0f, 2f)]
     private RangedFloat pitch = new RangedFloat(1, 1);
+    [SerializeField]
+    [MinMaxRange(0f, 1000f)]
+    private RangedFloat distance = new RangedFloat ( 1,1000);
+    [SerializeField]
+    private AudioMixerGroup mixer; 
 
 
     public void Play(AudioSource source)
     {
+        source.outputAudioMixerGroup = mixer;
+    
         int clipIndex = UnityEngine.Random.Range(0, clips.Length);
         source.clip = clips[clipIndex];
+
+        source.pitch = Random.Range(pitch.minValue, pitch.maxValue);
+        source.volume = Random.Range(volume.minValue, volume.maxValue);
+
+        source.minDistance = distance.minValue;
+        source.maxDistance = distance.maxValue;
+
         source.Play();
     }
 }
